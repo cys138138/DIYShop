c73bda60992898de5beaf7270c40032b41184e5c
jayhbb <792599135@qq.com>
2016-08-02 13:15:12 +0800

    uplower file name
    uplower file name

--- code/diyshop/rebuild/common/model/mobileVerify.php
+++ code/diyshop/rebuild/common/model/mobileVerify.php
@@ -7,6 +7,6 @@ use umeworld\lib\Query;
 class MobileVerify extends \common\lib\DbOrmModel{
 
 	public static function tableName(){
-		return Yii::$app->db->parseTable('_@mobile_verify');
+		return Yii::$app->db->parseTable('_@mobile_verify'); 
 	}
 }
