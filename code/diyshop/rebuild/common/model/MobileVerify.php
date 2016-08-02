948be7b9fa160bec01cf7a87a2cde0ee04ca34d8
jayhbb <792599135@qq.com>
2016-08-02 13:31:04 +0800

    update
    update

--- code/diyshop/rebuild/common/model/MobileVerify.php
+++ code/diyshop/rebuild/common/model/MobileVerify.php
@@ -2,6 +2,7 @@
 namespace common\model;
 use Yii;
 use umeworld\lib\Query;
+
 class MobileVerify extends \common\lib\DbOrmModel{
 	public static function tableName(){
 		return Yii::$app->db->parseTable('_@mobile_verify'); 
