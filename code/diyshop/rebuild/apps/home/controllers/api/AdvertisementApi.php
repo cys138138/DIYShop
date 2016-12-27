3a01e463634d3bc30057cd6a29f9daf4945ae839
jayhbb <792599135@qq.com>
2016-12-27 10:52:13 +0800

    update advertisement api
    update advertisement api

--- code/diyshop/rebuild/apps/home/controllers/api/AdvertisementApi.php
+++ code/diyshop/rebuild/apps/home/controllers/api/AdvertisementApi.php
@@ -13,7 +13,26 @@ trait AdvertisementApi{
 		$isShowAdvertisement = Setting::getSetting(Setting::IS_SHOW_ADVERTISEMENT);
 		$aTopAdertisement = json_decode(Setting::getSetting(Setting::TOP_ADVERTISEMENT), true);
 		$aBgAdertisement = json_decode(Setting::getSetting(Setting::BG_ADVERTISEMENT), true);
-		
+		$aReturn = [
+			'top' => $aTopAdertisement,
+			'bg' => $aBgAdertisement,
+			'is_show_advertisement' => $isShowAdvertisement,
+		];
+		if(Yii::$app->request->post('version') == '1.0.0'){
+			$aOldTopAdertisement = [];
+			foreach($aTopAdertisement as $aValue){
+				array_push($aOldTopAdertisement, $aValue['pic']);
+			}
+			$aOldBgAdertisement = [];
+			foreach($aBgAdertisement as $aValue){
+				array_push($aOldBgAdertisement, $aValue['pic']);
+			}
+			$aReturn = [
+				'top' => $aOldTopAdertisement,
+				'bg' => $aOldBgAdertisement,
+				'is_show_advertisement' => $isShowAdvertisement,
+			];
+		}
 		return new Response('首页广告图片', 1, [
 			'top' => $aTopAdertisement,
 			'bg' => $aBgAdertisement,
